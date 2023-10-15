7bc7dd0d02a57151dfcf3be79a09dcd399cc4e1a
Allan Rocha <73902180+Allankrc@users.noreply.github.com>
2023-10-15 17:16:01 -0300

    Alteração de estrutura- não salvei no xampp httdoc

---
+++ agroroxico.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+
+<form id="">
+    <label> precipitacao mm/ano</label>
+    <label> tx irrrigação mm/ano</label>
+    <label> evapotranspiracao mm/ano</label>
+</form>
+
+<form id="aquifero">
+    <label> porosidade v/v</label>
+    <label> profundidade mm/ano</label>
+</form> 
+<form>   
+    <label> evapotranspiracao mm/ano</label>
+    <label> evapotranspiracao mm/ano</label>
+    <label> evapotranspiracao mm/ano</label>
+    <label> evapotranspiracao mm/ano</label>
+
+</form>
