5db9e649975feb5604dd461106336c9761fc9992
Allan <allankrc@gmail.com>
2023-10-19 21:18:27 -0300

    Adicionada as formuas em python

--- Risco_agroroxico.php
+++ Risco_agroroxico.php
@@ -6,7 +6,17 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     
-<h1> 1 - Caracterizações da área de estudos </h1>
+
+<?php
+    require '/solo_local.php';
+    require '/solo_carc.php';
+    require '/agrotoxico.php';
+    require '/resultado.php';
+
+?>
+
+
+<h1> 1 - Caracterizaçõ   de estudos </h1>
 
 <div class="mb-3">
 <form id="">
@@ -76,3 +86,4 @@
 
 
 <h1>Agoróxico</h1>
+
---
+++ index.php
@@ -0,0 +1,12 @@
+
+
+
+<?php 
+
+
+require '/agrotoxico.php';
+require '/outroproblema.php';
+require '/problema2.php';
+require '/mais_problema.php';
+
+?>
